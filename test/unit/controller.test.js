6b375b832c43927bc0b8e0b2773ac4bd72b6a7f3
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-04-04 00:55:40 +0200

    test(controller): add sample test

---
+++ test/unit/controller.test.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+
+import createController from '../../src/controller';
+
+test.todo('listFloors - should list floors and return 200 OK');
+
+test.todo('getFloor - should get a floor by ID and return 200 OK');
+
+test.todo('listAreas - should list a floor\'s areas and return 200 OK');
+
+test.todo('getArea - should get a floor\'s area and return 200 OK');
