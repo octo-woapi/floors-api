08769f91ec4f05d7c8bf2a681a6ad306fcc535ac
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-04-08 22:22:09 +0200

    test(repository): add repository tests

--- test/unit/controller.test.js
+++ test/unit/controller.test.js
@@ -1,10 +1,76 @@
 import test from 'ava';
+import sinon from 'sinon';
 
 import createController from '../../src/controller';
 
-test.todo('listFloors - should list floors and return 200 OK');
+const floors = [
+  {
+    id: 2,
+    areas: [
+      {
+        id: 1,
+        name: 'Salle Tim Berners-Lee',
+        capacity: 10,
+      },
+      {
+        id: 2,
+        name: 'Salle Vint Cerf',
+        capacity: 4,
+      },
+      {
+        id: 3,
+        name: 'Open space Opéra',
+        surface: 272,
+        capacity: 50,
+      },
+    ],
+  },
+];
 
-test.todo('getFloor - should get a floor by ID and return 200 OK');
+const repository = {
+  findFloors: sinon.stub().resolves(floors),
+  findFloorById: sinon.stub(),
+};
+repository.findFloorById.withArgs(2).resolves(floors[0]);
+const controller = createController(repository);
+
+test('listFloors - should list floors and return 200 OK', async (t) => {
+  const ctx = {};
+  await controller.listFloors(ctx);
+  t.is(ctx.status, 200);
+  t.true(Array.isArray(ctx.body));
+  t.true(ctx.body.length > 0);
+});
+
+test('getFloor - should get a floor by ID and return 200 OK', async (t) => {
+  const ctx = {
+    path: {
+      id: 2,
+    },
+  };
+  await controller.getFloor(ctx);
+  t.is(ctx.status, 200);
+  t.is(ctx.body.id, 2);
+  t.is(ctx.body.areas.length, 3);
+});
+
+test('getFloor - should throw a NotFound error if the floor was not found', async (t) => {
+  const ctx = {
+    path: {
+      id: 42,
+    },
+    throw() {
+      const err = new Error('Not found');
+      err.status = 404;
+      err.expose = true;
+      throw err;
+    },
+  };
+  const promise = controller.getFloor(ctx);
+  const err = await t.throws(promise);
+  t.is(typeof err, 'object');
+  t.is(err.status, 404);
+});
 
 test.todo('listAreas - should list a floor\'s areas and return 200 OK');
 
---
+++ test/unit/repository.test.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+import path from 'path';
+
+import createRepository from '../../src/repository';
+
+const file = path.join(__dirname, '..', '..', 'assets', 'floors.json');
+const repository = createRepository(file);
+
+test('findFloors - should find floors', async (t) => {
+  const floors = await repository.findFloors();
+  t.true(Array.isArray(floors));
+  t.true(floors.length > 0);
+});
+
+test('findFloor - should find one floor', async (t) => {
+  const floor = await repository.findFloorById(2);
+  t.is(typeof floor, 'object');
+  t.is(floor.id, 2);
+  t.true(floor.areas.length > 0);
+});
+
+test('findFloorAreas - should find a floor\'s areas', async (t) => {
+  const areas = await repository.findFloorAreas(2);
+  t.true(Array.isArray(areas));
+  t.true(areas.length > 0);
+});
+
+test('findFloorAreaById - should a floor\'s specific area by Id', async (t) => {
+  const area = await repository.findFloorAreaById(2, 1);
+  t.is(typeof area, 'object');
+  t.is(typeof area.name, 'string');
+  t.is(typeof area.capacity, 'number');
+  t.is(area.id, 1);
+});
