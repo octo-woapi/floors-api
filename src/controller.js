2538985bd612ff2ff63882f24299a9e5e6a0f47a
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2018-04-08 22:22:44 +0200

    feat(controller): implement floor methods

---
+++ src/controller.js
@@ -0,0 +1,21 @@
+const listFloors = repository =>
+  async (ctx) => {
+    const floors = await repository.findFloors();
+    ctx.status = 200;
+    ctx.body = floors;
+  };
+
+const getFloor = repository =>
+  async (ctx) => {
+    const floor = await repository.findFloorById(ctx.path.id);
+    if (!floor) {
+      ctx.throw(404, 'Floor not found');
+    }
+    ctx.status = 200;
+    ctx.body = floor;
+  };
+
+module.exports = repository => ({
+  listFloors: listFloors(repository),
+  getFloor: getFloor(repository),
+});
